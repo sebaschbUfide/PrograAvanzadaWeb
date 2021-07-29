c8dc762caa6fdfc17b2d7a532b5c3e7bf6b15c9f
Stevenjm01 <68606227+Stevenjm01@users.noreply.github.com>
2021-07-29 14:46:21 -0600

    Diseño de carrito de compras

--- ProyectoPrograAvansada/FontEnd/Controllers/HomeController.cs
+++ ProyectoPrograAvansada/FontEnd/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace FontEnd.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
     }
 }
