e270e0aa4a1d7413e3362abbc84418cdf67163ce
Chescy <francesco309810@hotmail.com>
2021-08-21 09:30:34 -0600

    Interfaz y estilos

--- ProyectoPrograAvansada/FrontEndApi/Startup.cs
+++ ProyectoPrograAvansada/FrontEndApi/Startup.cs
@@ -46,7 +46,7 @@ namespace FrontEndApi
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Producto}/{action=Index}/{id?}");
             });
         }
     }
